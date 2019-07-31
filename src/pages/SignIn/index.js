a2bc0b496b1f2f66f9145d2e39d2964676c10c29
Joseph <joseph.kal@outlook.com>
2019-07-31 20:14:24 -0300

    added unform lib to handle input data

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="meetup" />
 
-      <form>
-        <input type="email" placeholder="Your awesome email" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your awesome email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
 
         <button type="submit">Log In!</button>
         <Link to="/register">Create an awesome account!</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="meetup" />
 
-      <form>
-        <input placeholder="What's your full name?" />
-        <input type="email" placeholder="Your awesome email" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name " placeholder="What's your full name?" />
+        <Input name="email" type="email" placeholder="Your awesome email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
 
         <button type="submit">Create awesome account!</button>
         <Link to="/">I am registred!</Link>
-      </form>
+      </Form>
     </>
   );
 }
