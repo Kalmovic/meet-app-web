7419375966c2d589c55ee3debc4d6886f5b10efe
Joseph <joseph.kal@outlook.com>
2019-07-31 15:29:45 -0300

    setting routes and reactotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history'; // Enable navigation from anywhere of the code
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sup Joseph</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
