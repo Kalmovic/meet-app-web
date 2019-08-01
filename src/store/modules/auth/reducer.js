2456e691777f21d5fd8e55e41c0d5c1c6a3e7b5f
Joseph <joseph.kal@outlook.com>
2019-07-31 22:57:12 -0300

    added redux and saga configuration

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {};
+
+// return the updated state according to the action taken
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
