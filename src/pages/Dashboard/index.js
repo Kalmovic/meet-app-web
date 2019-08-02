79a24de5618cacd20f17403277b7e20ac50ddb70
Joseph <joseph.kal@outlook.com>
2019-08-02 11:30:30 -0300

    meetups are being listed but when refreshing the page they are gone

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+
+import { Container, Title, List, Meet } from './styles';
 import api from '~/services/api';
 
-// import { Container } from './styles';
-
 export default function Dashboard() {
-  api.get('mymeetups');
-  return <h1>dashboard</h1>;
+  const [meetup, setMeetup] = useState([]);
+  useEffect(() => {
+    async function loadMeetup() {
+      const response = await api.get('mymeetups');
+      console.tron.log(response);
+      setMeetup(response.data);
+    }
+    loadMeetup();
+  }, []);
+
+  return (
+    <Container>
+      <Title>
+        <h1>My meetups</h1>
+        <button type="button">New meetup</button>
+      </Title>
+      <List>
+        <ul>
+          {meetup.map(meet => (
+            <Meet key={meet.id} past={meet.past}>
+              <strong>{meet.title}</strong>
+              <span>{meet.date}</span>
+            </Meet>
+          ))}
+        </ul>
+      </List>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    display: flex;
+    color: #fff;
+    font-size: 30px;
+  }
+
+  button {
+    margin: 5px 0 0;
+    height: 40px;
+    width: 140px;
+    background: #f94d6a;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 14px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.05, '#f94d6a')};
+    }
+  }
+`;
+
+export const List = styled.div`
+  margin-top: 40px;
+  list-style: none;
+`;
+
+export const Meet = styled.button`
+  border: none;
+  width: 100%;
+  margin-bottom: 5px;
+  display: flex;
+  padding: 10px 20px;
+  border-radius: 4px;
+  background: #2b1c31;
+  justify-content: space-between;
+
+  strong {
+    color: #fff;
+  }
+
+  span {
+    color: #eee;
+  }
+`;
