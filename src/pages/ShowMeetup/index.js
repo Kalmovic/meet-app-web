5858d620aeeabd839705c04317113eb24a302710
Joseph <joseph.kal@outlook.com>
2019-08-04 19:03:12 -0300

    delete meetup done

--- src/pages/ShowMeetup/index.js
+++ src/pages/ShowMeetup/index.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { parseISO, format } from 'date-fns';
 import { MdEdit, MdDeleteForever, MdEvent, MdLocationOn } from 'react-icons/md';
+import { toast } from 'react-toastify';
 import { Container, Title, Footer, Body } from './styles';
 import api from '~/services/api';
+import history from '~/services/history';
 
 export default function ShowMeetup({ match }) {
   const [meetId] = useState(match.params.id);
@@ -25,18 +27,29 @@ export default function ShowMeetup({ match }) {
     loadMeetup();
   }, [meetId]);
 
+  async function handleDelete() {
+    try {
+      await api.delete(`meetups/${meetId}`);
+      toast.success('Meetup deleted with success!');
+      history.push('/dashboard');
+    } catch (err) {
+      console.tron.log(err);
+      toast.error('Meetup could not be deleted');
+    }
+  }
+
   return (
     <Container>
       <Title>
         <h1>{meetup.title}</h1>
         <nav>
-          <button id="edit" type="submit">
+          <button id="edit" type="button">
             <div>
               <MdEdit size={18} color="#fff" />
               Edit
             </div>
           </button>
-          <button id="delete" type="submit">
+          <button id="delete" type="button" onClick={handleDelete}>
             <div>
               <MdDeleteForever size={18} color="#fff" />
               Delete
@@ -64,5 +77,4 @@ export default function ShowMeetup({ match }) {
       </Footer>
     </Container>
   );
-  /**  */
 }
